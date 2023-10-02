3f89e3449a643c12056490b148cd217ae50f0df0
Ak_D7 <akshitadeka7@gmail.com>
2023-10-02 22:26:47 +0530

    auth

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,4 +1,33 @@
-const isAuthenticated = ()=>{
-    
+import jwt from "jsonwebtoken";
+import Auser from "../models/Auser.js";
+import Muser from "../models/Muser.js";
+
+export const isAutha = async(req, res, next)=>{
+    const {token} = req.cookies;
+
+    if(!token){
+        return res.status(404).json({
+            success : false,
+            message : "Login First"
+        })
+    }
+
+    const decodedId = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = await Auser.findById(decodedId._id);
+    next();
 }
-export default isAuthenticated;
+export const isAuthm = async(req, res, next)=>{
+    const {token} = req.cookies;
+
+    if(!token){
+        return res.status(404).json({
+            success : false,
+            message : "Login First"
+        })
+    }
+
+    const decodedId = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = await Muser.findById(decodedId._id);
+    next();
+}
+
